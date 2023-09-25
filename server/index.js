8654781c59eae628b5f0e04655274f43853509c2
sashacamar <69942870+sashacamar@users.noreply.github.com>
2023-09-25 01:37:48 -0300

    otras mas

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const { sequelize } = require("./src/db");
 const { PORT } = process.env;
 
 app.listen(3001, () => {
-    sequelize.sync({ alter:  true });
+    sequelize.sync({ force:  true });
     console.log(`Listening on port ${PORT}`)
 })
--- server/src/app.js
+++ server/src/app.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cookieParser());
-app.use(morgan('dev'));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
