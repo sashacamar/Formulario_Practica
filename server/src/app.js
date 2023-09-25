54ba3abb172bada7ceefd4943ddda35371e56679
sashacamar <69942870+sashacamar@users.noreply.github.com>
2023-09-25 01:30:48 -0300

    mas cosas

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
 const mainRouter = require("./routes");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cookieParser());
+server.use(morgan('dev'));
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 
 app.use(mainRouter);
 
