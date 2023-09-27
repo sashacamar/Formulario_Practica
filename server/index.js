79fbb786eea099d65dbcb78d0605eeeefc7a9caf
sashacamar <69942870+sashacamar@users.noreply.github.com>
2023-09-27 16:58:13 -0300

    cambio de force a alter

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const { sequelize } = require("./src/db");
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
-    sequelize.sync({ force:  true });
+    sequelize.sync({ alter:  true });
     console.log(`Listening on port ${PORT}`)
 })
